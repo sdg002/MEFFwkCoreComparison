a77322bc5ff6da5a0f713e05301f45f54cb76b01
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-09-18 17:06:25 +0100

    Added Program2.cs which demonstrates the same problem with Assembly.LoadFrom

---
+++ ConsoleAppNETFramework/Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNETFramework
+{
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Console.WriteLine("Enter the folder which contains the folders with Plugins");
+                //string folderWithPlugins = Console.ReadLine();
+                string folderNewtonsoftV9 = @"C:\Users\saurabhd\MyTrials\OtherStuff\MEFFwkCoreComparison\PluginsDeliveryFolder\out\PluginNewtonsoftv9\PluginNewtonsoftv9.dll";
+                string folderNewtonsoftV12 = @"C:\Users\saurabhd\MyTrials\OtherStuff\MEFFwkCoreComparison\PluginsDeliveryFolder\out\PluginNewtonsoftV12\PluginNewtonsoftv12.dll";
+                string[] pathsAssemblies = new string[] { folderNewtonsoftV9, folderNewtonsoftV12 };
+                foreach (string pathAssembly in pathsAssemblies)
+                {
+                    var assembly = System.Reflection.Assembly.LoadFrom(pathAssembly);
+                    Type tTarget = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower() == "class1");
+                    var plugin = Activator.CreateInstance(tTarget) as Contracts.ISomePlugin;
+                    plugin.DoWork();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
